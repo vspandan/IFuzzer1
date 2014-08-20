bf18476ca7d04c793be7e3c4b647e26cd314faec
Spandan Veggalam <spandan.veggalam@gmail.com>
2014-08-20 23:40:46 +0530

    Commiting changes "20/08"
    Signed-off-by: Spandan Veggalam <spandan.veggalam@gmail.com>

--- src/python/GenerateLexInput.py
+++ src/python/GenerateLexInput.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+from os import system
 
 grammar_files_path = "../../GrammarFiles"
 grammar_files = [ f for f in listdir(grammar_files_path) if isfile(join(grammar_files_path, f)) ]
@@ -23,37 +24,56 @@ def _msg():
     except ValueError:
         print "\nInvalid Input"
         _msg()
+        
+def _extractKeyWords():
+    with open(grammar_file, 'r') as f1:
+        bnf = f1.read()
+        for item in bnf.split('\n'):
+            if item.find('::=') >= 0:
+                key, values = item.split('::=')
+                key = key.strip()
+                if key == "KEYWORD" :
+                    return values.split('|')
+
 
 def _createLexInput():
 
     with open("../../output/lexfile.l", 'w') as f:
         
-        #definitions Begin
-        f.write("/***definitions***/ \n")
-        f.write("digit   [0-9] \n" )
-        f.write("delimiter   [\t\n :;] \n" )
-        f.write("ws   {delimiter}+ \n" )
-        f.write("letter   [A-Za-z] \n")
-        f.write("id   {letter}({letter}|{digit})* \n")
-        f.write("number   {digit}+(\.{digit}+)?(E[+-]?{digit}+)? \n")
-        #definitions End
+        # definitions Begin"""
+        f.write("/***definitions***/ \n")        
+        with open("../../other/ConstantDeclarations", 'r') as f1:
+            f.write(f1.read())
+        # definitions End"""
         
-        #Rules Begin
-        f.write("\n%%\n")
-        f.write("/***rules***/ \n")
-        f.write(".   {ECHO; yyerror(\"unexpected character\");}")
+        # Rules Begin"""
+        f.write("\n%%\n")        
+        values=_extractKeyWords()
+        for value  in values:
+            f.write("\""+value.strip()+"\"\t\t{ return("+value.strip().upper()+"); }\n") 
         
-        #Rules End
         
-        #User Code Begins
+        
+        ## Constant Rules 
+        with open("../../other/ConstantRules", 'r') as f1:
+            f.write(f1.read())
+        # Rules End"""
+        
+        # User Code Begins"""
         f.write("\n%%\n")
         f.write("/***user code***/ \n")
-        #User Code Ends
+        # User Code Ends"""
                 
 grammar_file = _msg()
-# print grammar_file
+print grammar_file
 
 _createLexInput()
+
+#Flex
+system("flex ../../output/lexfile.l");
+system("mv lex.yy.c ../../output/lex.yy.c");
+
+#Yacc
         
         
         
