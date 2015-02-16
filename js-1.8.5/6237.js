536d45ccc2c4d9ff64c89061d27ca75d97a49e23
Spandan Veggalam <spandan.veggalam@gmail.com>
2015-02-16 14:27:15 +0530

    Updated repo till date

---
+++ js-1.8.5/6237.js
@@ -0,0 +1,12 @@
+var IN_RHINO = inRhino ( ) ; 
+if ( ! IN_RHINO ) {
+	var a1 = Array ( 0xFFFFFFFF ) ; 
+	a1 . sort ( ) ; 
+	a1 = null ; 
+} 
+var a2 = Array ( 0x40000000 ) ; 
+a2 . sort ( ) ; 
+a2 = null ; 
+var a3 = Array ( 0x10000000 / 3 ) ; 
+a3 . sort ( ) ; 
+a3 = null ; 
